bbbf42220a005679c7971761e5ac040ad4721fca
lilanjie <18512448197@163.com>
2018-01-06 21:42:53 +0800

    2018.1.5

---
+++ 2018.1.5(1)/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"name: ";
+    cin>>str;
+    cout <<"hello "<<str<< endl;
+    while(1);
+    return 0;
+}
